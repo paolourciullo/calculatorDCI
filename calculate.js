461dc95423db6e82caadda03986b0b5358b9edbd
Paolo U <paolourciullo@gmail.com>
2021-05-02 13:20:52 -0400

    add eval method

--- calculate.js
+++ calculate.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
   $('.inputElements').on('click', function () {
     $('#calculatorInput').append($(this).text());
   });
+  $('.clear').on('click', function () {
+    $('#calculatorInput').empty();
+  });
+  $('.evaluate').on('click', function () {
+    eval($('#calculatorInput').val());
+  });
 });
